b85f28cf8e30d35089ca4ddedc1812b5c58affe6
sin wan wildy <sw.wildy@gmail.com>
2020-11-20 11:20:09 -0800

    part2

--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,24 @@
 class VentureCapitalist
 
+    attr_accessor :name, :total_worth
+    #attr_reader
+
+    @@all = []
+
+    def initialize(name, total_worth)
+        @name = name 
+        @total_worth = total_worth 
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.tres_commas_club
+        self.all.select do |vc|
+            vc.total_worth >= 1000000000
+        end
+    end
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,10 @@ s1 = Startup.new("Keyboard Company", "Emma Fewer", "www.keyboards.com")
 s2 = Startup.new("Coffee Time", "Rachel Cross", "coffeetime.com")
 s3 = Startup.new("Tea Time", "Sin Wildy", "teatime.com")
 
+vc1 = VentureCapitalist.new("Emma 2", 88888)
+vc2 = VentureCapitalist.new("Rachel 2", 1000000009)
+vc3 = VentureCapitalist.new("Sin 2", 999)
+
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
