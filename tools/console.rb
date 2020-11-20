01c0981e175d9c1dc3e2a8578b588cb7d8f64b84
emmafewer <emmafewer20@gmail.com>
2020-11-20 11:03:00 -0800

    done with Startup initialize

--- tools/console.rb
+++ tools/console.rb
@@ -3,9 +3,9 @@ require_relative '../config/environment.rb'
 def reload
   load 'config/environment.rb'
 end
-# Insert code here to run before hitting the binding.pry
-# This is a convenient place to define variables and/or set up new object instances,
-# so they will be available to test and play around with in your console
+s1 = Startup.new("Keyboard Company", "Emma Fewer", "www.keyboards.com")
+s2 = Startup.new("Coffee Time", "Rachel Cross", "coffeetime.com")
+s3 = Startup.new("Tea Time", "Sin Wildy", "teatime.com")
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
